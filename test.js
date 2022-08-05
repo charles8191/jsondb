a7982272c71fbc8d715829ae982c1f1528eefcab
wumpus <wumpus@noreply.codeberg.org>
2022-08-05 21:09:31 +0200

    Update 'test.js'

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-let { loadDB } = require("./index.js");
-let db = loadDB("test.json");
-console.log(db.test);
+let { DB } = require("./index.js");
+let { readFileSync } = require("fs");
+let db = new DB("test.json");
+db.v.test = "If you see this and a JSON object, it works."
+db.update();
+console.log(db.v.test);
+console.log(readFileSync("test.json", "utf8"));
