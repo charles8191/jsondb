866b5a1ef411efddbf33a648a0fc3e562d78297b
wumpus <wumpus@noreply.codeberg.org>
2022-08-05 21:04:37 +0200

    Update 'index.js'

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 let fs = require("fs");
-function DB(databaseFile) {
-  let _db;
-  if (fs.existsSync(databaseFile)) {
-    _db = JSON.parse(fs.readFileSync(databaseFile, "utf8"));
-  } else {
-    fs.writeFileSync(databaseFile, JSON.stringify({}));
-    _db = {};
-  }
-  return class {
-    constructor() {
+class DB {
+    constructor(databaseFile) {
+      let _db;
+      if (fs.existsSync(databaseFile)) {
+        _db = JSON.parse(fs.readFileSync(databaseFile, "utf8"));
+      } else {
+        fs.writeFileSync(databaseFile, JSON.stringify({}));
+        _db = {};
+      }
       this.v = _db
       this.update = function() {
         fs.writeFileSync(databaseFile, JSON.stringify(this.v));
       }
-    }
   }
 }
 exports.DB = DB;
